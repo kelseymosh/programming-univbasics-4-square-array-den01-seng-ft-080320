57c0ecee2608110a766ed5a097d1a72a489d4139
Kelsey Oshiro <kelseymosh@gmail.com>
2020-06-23 16:57:21 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,7 @@
-def square_array(numbers)
-  numbers.each do |num|
-    new_numbers << (num ** 2)
+def square_array(array)
+  array.each do |num|
+  new_numbers << (array ** 2)
   end
   return new_numbers
 end
+ 
